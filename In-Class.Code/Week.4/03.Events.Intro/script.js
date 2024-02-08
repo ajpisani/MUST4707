bbe02e6d3237f907c54a19a21fadeb01dc4d5f54
ajpisani <156853937+ajpisani@users.noreply.github.com>
2024-02-08 15:35:40 -0500

    Update script.js

--- In-Class.Code/Week.4/03.Events.Intro/script.js
+++ In-Class.Code/Week.4/03.Events.Intro/script.js
@@ -1,1 +1,14 @@
 //Event Interaction
+
+const button_click = function () {
+  let usr_input = document.getElementById("userInput");
+  console.log(usr_input.value);
+};
+
+const do_stuff = function () {
+  console.log("stuff");
+  let button = document.getElementById("displayBtn");
+  button.addEventListener("click", button_click);
+};
+
+document.addEventListener("DOMContentLoaded", do_stuff);
